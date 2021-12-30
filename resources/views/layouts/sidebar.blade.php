2ccdcab6465547f01664354aceacdf13c675b2e2
RyVisan <visantry20@gmail.com>
2021-12-30 12:29:43 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,17 +4,79 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
         <li class="header">POINT OF SALE</li>
-        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Home</span></a></li>
-        <li class="active treeview">
+        <li  class="active"><a href="#" ><i class="fa fa-home" aria-hidden="true"></i> <span>Home</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Sale</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Sale Transaction</span></a></li>
+        <li class="header">ACCOUNTANT</li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Invoice</span></a></li>
+        <li class="treeview">
           <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+            <i class="fa fa-edit"></i> <span>Expense</span>
+            <i class="fa fa-angle-left pull-right"></i>
           </a>
-          <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+          <ul class="treeview-menu" style="display: none;">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Expense</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Expense Category</a></li>
           </ul>
         </li>
-        <li><a href="{{ asset('assets') }}/documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Income</span>
+            <i class="fa fa-angle-left pull-right"></i>
+          </a>
+          <ul class="treeview-menu" style="display: none;">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Income</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Income Category</a></li>
+          </ul>
+        </li>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Inventory</span>
+            <i class="fa fa-angle-left pull-right"></i>
+          </a>
+          <ul class="treeview-menu" style="display: none;">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Stock In</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Stock Out</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Stock Adjustment</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Stock Balance</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Report</span>
+            <i class="fa fa-angle-left pull-right"></i>
+          </a>
+          <ul class="treeview-menu" style="display: none;">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Sale</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Revenue</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Expense</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Profit Loss</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Inventory</a></li>
+          </ul>
+        </li>
+        <li class="header">MAIN DATA</li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Product</span>
+            <i class="fa fa-angle-left pull-right"></i>
+          </a>
+          <ul class="treeview-menu" style="display: none;">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Product</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Product Category</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Customer</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Supplier</span></a></li>
+        <li class="header">MANAGEMENT</li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>User</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Roles</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Abilities</span></a></li>
+        <li class="header">DEVELOPER MODE</li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Setting</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Currency</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Payment Method</span></a></li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Data Backup</span></a></li>
       </ul>
     </section>
     <!-- /.sidebar -->
