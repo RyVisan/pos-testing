c7768764f2409786f7f6732f4e2d6af1cd26cd12
RyVisan <visantry20@gmail.com>
2021-12-30 11:18:46 +0700

    Upload project and condig layouts

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('content')
+    <section class="content-header">
+        <h1>
+        Dashboard
+        <small>Control panel</small>
+        </h1>
+        <ol class="breadcrumb">
+        <li><a href="#"><button class="btn btn-primary"><i class='bx bx-plus'></i> Create</button></a></li>
+        </ol>
+    </section>
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,21 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">POINT OF SALE</li>
+        <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> <span>Home</span></a></li>
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+          </ul>
+        </li>
+        <li><a href="{{ asset('assets') }}/documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
